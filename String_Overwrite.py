01b3f0416ddbe86d0eb434c0ed72376cd0d98c80
VIJAY YADAV <vy945716@gmail.com>
2024-08-03 20:49:22 +0530

    added new prog.

---
+++ String_Overwrite.py
@@ -0,0 +1,23 @@
+string = "Python is fun"
+
+# 'is' separator is found
+print(string.partition('is '))
+
+# 'not' separator is not found
+print(string.partition('not '))
+
+string = "Python is fun, isn't it"
+
+# splits at first occurence of 'is'
+print(string.partition('is'))
+
+--------------------
+song = 'cold, cold heart'
+
+# replacing 'cold' with 'hurt'
+print(song.replace('cold', 'hurt'))
+
+song = 'Let it be, let it be, let it be, let it be'
+
+# replacing only two occurrences of 'let'
+print(song.replace('let', "don't let",3))
