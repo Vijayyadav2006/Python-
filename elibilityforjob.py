29a71ec7c15288d6436a1b3e256135a38220c429
VIJAY YADAV <vy945716@gmail.com>
2024-08-03 10:21:41 +0530

    added new program

---
+++ elibilityforjob.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age:"))
+if age>=16 and age<18:
+   print("you are eligible for part time job")
+elif age>=18 and age<65:
+   print("you are eligible for full time job")
+else:
+   print("you are not eligible")
