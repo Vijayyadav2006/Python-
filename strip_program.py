67efe35ad493e701c875b06367501fa7a479c002
VIJAY YADAV <vy945716@gmail.com>
2024-08-03 20:53:33 +0530

    added new program

---
+++ strip_program.py
@@ -0,0 +1,10 @@
+title = '         Python     test          '
+
+# remove trailing whitespace from title
+print(title)
+result = title.strip() # strip blanckspace from both sides
+result1 = title.lstrip()
+result2 = title.rstrip()
+print("only strip func",result)
+print("only lstrip func",result1)
+print("only rstrip func",result2)
