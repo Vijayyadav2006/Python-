cddd4cd077b42f946534bb68ed13829f44251641
VIJAY YADAV <vy945716@gmail.com>
2024-08-03 20:50:51 +0530

    added

---
+++ replacing_program.py
@@ -0,0 +1,9 @@
+song = 'cold, cold heart'
+
+# replacing 'cold' with 'hurt'
+print(song.replace('cold', 'hurt'))
+
+song = 'Let it be, let it be, let it be, let it be'
+
+# replacing only two occurrences of 'let'
+print(song.replace('let', "don't let",3))
