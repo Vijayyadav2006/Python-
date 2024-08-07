6cd7e042463e9e0dde1d390dce8797302322695d
VIJAY YADAV <vy945716@gmail.com>
2024-08-07 12:42:50 +0530

    added

---
+++ Number_GreaterOr_Equal.py
@@ -0,0 +1,10 @@
+num=int(input("num")
+
+if num >0:
+        if num ==0:
+                   print("zero")
+        else:
+             print("Positive Number")
+else:
+        print(" Negative Number")
+        
