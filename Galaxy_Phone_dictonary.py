9e5dbad088a664da682103385d5c41fd9f03563b
VIJAY YADAV <vy945716@gmail.com>
2024-08-27 12:17:38 +0530

    Create Galaxy_Phone_dictonary.py

---
+++ Galaxy_Phone_dictonary.py
@@ -0,0 +1,8 @@
+Galaxy_35={
+"Brand_Name" : " oppo a37f",
+("Ram","Memory") : ["8GB","16GB"]["120GB", "256GB"],
+"CPU_SPEED" : "2.4 MZ",
+"Camera": "69Mp",
+"Operating system": " ANdroad 10"
+ }
+print(Galaxy_35)
